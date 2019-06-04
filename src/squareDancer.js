a023d1ddc3b339ab668ec1d1c239bb1239e8dcac
bpdavitt <bpdavitt@gmail.com>
2019-06-04 11:13:31 -0500

    Correct SquareDancer positioning accounting for cowboy image

--- src/squareDancer.js
+++ src/squareDancer.js
@@ -2,10 +2,27 @@ var SquareDancer = function(top, left, timeBetweenSteps) {
   this.partner = this.choosePartner(window.dancers);
   this.partnerTop = formatPixels($(this.partner.$node).css('top'));
   this.partnerLeft = formatPixels($(this.partner.$node).css('left'));
-  this.top = this.partnerTop - 30;
-  this.left = this.partnerLeft; 
+  //this.top = this.partnerTop - 30;
+  //this.left = this.partnerLeft; 
+  this.top = {
+    top: this.partnerTop - 110,
+    left: this.partnerLeft - 40
+  };
+  this.right = {
+    top: this.partnerTop - 40,
+    left: this.partnerLeft + 30
+  };
+  this.bottom = {
+    top: this.partnerTop + 30,
+    left: this.partnerLeft - 40
+  };
+  this.left = {
+    top: this.partnerTop - 40,
+    left: this.partnerLeft - 110
+  };
+  this.currentPosition = this.top;
 
-  Dancer.call(this, this.top, this.left, timeBetweenSteps);
+  Dancer.call(this, this.top.top, this.top.left, timeBetweenSteps);
   this.$node.addClass("cowboy");
   
 };
@@ -16,28 +33,34 @@ SquareDancer.prototype.constructor = SquareDancer;
 SquareDancer.prototype.step = function(timeBetweenSteps) {
   Dancer.prototype.step.call(this, timeBetweenSteps);
 
-  var position = {
-    top: this.top,
-    left: this.left
-  };
   
-  if (this.top < this.partnerTop) {
-    position.top = this.partnerTop;
-    position.left = this.partnerLeft + 30;
-  } else if (this.left > this.partnerLeft) {
-    position.left = this.partnerLeft;
-    position.top = this.partnerTop + 30;
-  } else if (this.top > this.partnerTop) {
-    position.top = this.partnerTop;
-    position.left = this.partnerLeft - 30;
-  } else if (this.left < this.partnerLeft) {
-    position.left = this.partnerLeft;
-    position.top = this.partnerTop - 30;
+  if (this.currentPosition === this.top) {
+    this.currentPosition = this.right;    
+  } else if (this.currentPosition === this.right) {
+    this.currentPosition = this.bottom;    
+  } else if (this.currentPosition === this.bottom) {
+    this.currentPosition = this.left;    
+  } else if (this.currentPosition === this.left) {
+    this.currentPosition = this.top;    
   }
+
+  // if (this.top < this.partnerTop) {
+  //   position.top = this.partnerTop;
+  //   position.left = this.partnerLeft + 30;
+  // } else if (this.left > this.partnerLeft) {
+  //   position.left = this.partnerLeft;
+  //   position.top = this.partnerTop + 30;
+  // } else if (this.top > this.partnerTop) {
+  //   position.top = this.partnerTop;
+  //   position.left = this.partnerLeft - 30;
+  // } else if (this.left < this.partnerLeft) {
+  //   position.left = this.partnerLeft;
+  //   position.top = this.partnerTop - 30;
+  // }
   
-  this.top = position.top;
-  this.left = position.left;
-  this.setPosition(this.top, this.left);
+  // this.top = position.top;
+  // this.left = position.left;
+  this.setPosition(this.currentPosition.top, this.currentPosition.left);
   //begin any direction
   //compare the dancer and the partner's top/left
   //kick you on around whenever you are exceeding your partner's top, left, !top, !left
